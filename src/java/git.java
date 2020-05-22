9ff9a48cc13a12bf3e6512600b4bf1b2ca6fd0d0
lixiang <214637252@qq.com>
2020-05-22 17:33:50 +0800

    添加到git

---
+++ src/java/git.java
@@ -0,0 +1,12 @@
+package java;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("fafafafa");
+        System.out.println("fafafafa");
+        System.out.println("fafafafa");
+        System.out.println("fafafafa");
+        System.out.println("fafafafa");
+
+    }
+}
